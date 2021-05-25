17727ce16e9948e968cbd3a7e7c1a0ddd801e557
Wang, Jiyao <wangjiy@ncbi.nlm.nih.gov>
2021-05-25 16:28:12 -0400

    Switched residue numbers from integers to strings such as "100A".

--- build/icn3d.js
+++ build/icn3d.js
@@ -45030,11 +45030,12 @@ var icn3d = (function (exports) {
             html += "<li><span>Label Scale</span>";
             html += "<ul>";
 
-            for(var i = 1; i <= 9; ++i) {
-                html += me.htmlCls.setHtmlCls.getRadio('mn6_labelscale', 'mn6_labelscale0' + i, '0.' + i);
+            for(var i = 1; i <= 4; ++i) {
+                var twoi = 2 * i;
+                html += me.htmlCls.setHtmlCls.getRadio('mn6_labelscale', 'mn6_labelscale0' + twoi, '0.' + twoi);
             }
 
-            for(var i = 1; i <= 4; i *= 2) {
+            for(var i = 1; i <= 5; ++i) {
                 if(i == 1) {
                     html += me.htmlCls.setHtmlCls.getRadio('mn6_labelscale', 'mn6_labelscale' + i + '0', i + '.0', true);
                 }
