315ab49a94e475a72a0a45f422b2bef3427a2d97
Wang, Jiyao <wangjiy@ncbi.nlm.nih.gov>
2022-04-18 10:56:25 -0400

    Added "[comment]" in front of logs to diferentiate from commands in the command window. If "mmdbid" is the input and the parameter "bu" is not defined, set "bu" as 1 (biological unit).

--- dist/CHANGELOG.md
+++ dist/CHANGELOG.md
@@ -1,4 +1,6 @@
 ## Change Log
+[icn3d-3.11.2](https://www.ncbi.nlm.nih.gov/Structure/icn3d/icn3d-3.11.2.zip) was release on April 18, 2022. Added "[comment]" in front of logs to diferentiate from commands in the command window. If "mmdbid" is the input and the parameter "bu" is not defined, set "bu" as 1 (biological unit).
+
 [icn3d-3.11.1](https://www.ncbi.nlm.nih.gov/Structure/icn3d/icn3d-3.11.1.zip) was release on April 13, 2022. Users can input multiple PDB, MMDB, or AlphaFold IDs with the menu "File > Retrieve by ID > MMDB or AlphaFold IDs". Users can also load multiple PDB files at the same time with the menu "File > Open File > PDB Files (appendable)". The color legend became draggable.
 
 [icn3d-3.11.0](https://www.ncbi.nlm.nih.gov/Structure/icn3d/icn3d-3.11.0.zip) was release on March 28, 2022. Users can align AlphaFold structures or PDB structures with the menu "File > Align > Multiple Chains" or "File > Align > Structure to Structure > Two AlphaFold Structures". Users can also load any structures as usual, then load your custom PDB file with the menu "File > Open File > PDB File (appendable)", then relaign these structures with the meu "File > Realign Selection > by Structure Alignment".
--- dist/icn3d.js
+++ dist/icn3d.js
@@ -12851,6 +12851,11 @@ var icn3d = (function (exports) {
                    let command_tf2 = command_tf[0].split('&command=');
                    let commandStr = command_tf2[0].trim();
 
+                   // only one load command
+                   //if(prevCommandStr.substr(0, 5) == 'load ' && commandStr.substr(0, 5) == 'load ') {
+                   //    continue;
+                   //}
+
                    //statefile += ic.commands[i] + "\n";
 
                    // only output the most recent 'select saved atoms...' without " | name ..."
@@ -27791,7 +27796,7 @@ var icn3d = (function (exports) {
 
           ic.bRender = false;
           ic.bStopRotate = true;
-
+          
           // firebase dynamic links replace " " with "+". So convert it back
           dataStr =(bStatefile) ? dataStr.replace(/\+/g, ' ') : dataStr.replace(/\+/g, ' ').replace(/;/g, '\n');
 
@@ -28950,7 +28955,7 @@ var icn3d = (function (exports) {
             if(me.deferred !== undefined) me.deferred.resolve(); if(ic.deferred2 !== undefined) ic.deferred2.resolve();
         }
 
-        replayFirstStep(currentNumber) { let ic = this.icn3d; ic.icn3dui;
+        replayFirstStep(currentNumber) { let  ic = this.icn3d, me = ic.icn3dui;
               // fresh start
               ic.reinitAfterLoad();
               //ic.selectionCls.resetAll();
@@ -28971,6 +28976,8 @@ var icn3d = (function (exports) {
 
               $("#" + ic.pre + "replay_cmd").html('Cmd: ' + cmdStr);
               $("#" + ic.pre + "replay_menu").html('Menu: ' + menuStr);
+
+              me.htmlCls.clickMenuCls.setLogCmd(cmdStrOri, true);
 
               ic.bCommandLoad = false;
 
@@ -48793,7 +48800,8 @@ var icn3d = (function (exports) {
               }
           }
           if(ic.bAddLogs && me.cfg.showcommand) {
-              ic.logs.push(str);
+              let finalStr = (bSetCommand) ? str : '[comment] ' + str;
+              ic.logs.push(finalStr);
               // move cursor to the end, and scroll to the end
               $("#" + me.pre + "logtext").val("> " + ic.logs.join("\n> ") + "\n> ").scrollTop($("#" + me.pre + "logtext")[0].scrollHeight);
           }
@@ -51302,12 +51310,12 @@ var icn3d = (function (exports) {
         */
             html += "<div style='width:550px'>";
             html += "All chains will be aligned to the first chain in the comma-separated chain IDs. Each chain ID has the form of PDBID_chain (e.g., 1HHO_A, case sensitive) or UniprotID_chain (e.g., P69905_A for AlphaFold structures).<br/><br/>";
-            html += "<b>Chain IDs</b>: " + me.htmlCls.inputTextStr + "id='" + me.pre + "chainalignids' value='1HHO_A,4N7N_A,2HCO_A' size=50><br/><br/>";
+            html += "<b>Chain IDs</b>: " + me.htmlCls.inputTextStr + "id='" + me.pre + "chainalignids' value='P69905_A,P01942_A,1HHO_A' size=50><br/><br/>";
             html += "<b>Optional 1</b>, full chains are used for structure alignment<br/><br/>";
             html += "<b>Optional 2</b>, sequence alignment (followed by structure alignemnt) based on residue numbers in the First/Master chain: <br>" + me.htmlCls.inputTextStr + "id='" + me.pre + "resalignids' placeholder='1,5,10-50' size=50><br/><br/>";
             html += "<b>Optional 3</b>, predefined alignment with residue numbers in each chain specified (one chain per line): <br><textarea id='" + me.pre + "predefinedres' rows='5' style='width: 100%; height: " +(me.htmlCls.LOG_HEIGHT) + "px; padding: 0px; border: 0px;' placeholder='1,5,10-50\n1,5,10-50\n1,5,10-50'></textarea><br/><br/>";
             html += me.htmlCls.buttonStr + "reload_chainalign_asym'>Align Asymmetric Unit</button>" + me.htmlCls.buttonStr + "reload_chainalign' style='margin-left:30px'>Align Biological Unit</button><br/><br/>";
-            html += "(Note: To align chains in custom PDB files, you could concatenate PDB files in a single PDB file with the separation line \"ENDMDL\". Then load it in \"Open File > PDB File\" in the \"File\" menu and click \"View Sequences & Annotations\" in the \"Window\" menu. Finally select multiple chains in the sequence window and click \"Realign Selection\" in the \"File\" menu.)<br><br>";
+            html += "(Note: To align chains in custom PDB files, you could load them in \"File > Open File > PDB Files (appendable)\" and click \"Analysis > Defined Sets\". Finally select multiple chains in Defined Sets and click \"File > Realign Selection\".)<br><br>";
             html += "</div></div>";
 
             html += me.htmlCls.divStr + "dl_mutation' class='" + dialogClass + "'>";
@@ -53990,6 +53998,9 @@ var icn3d = (function (exports) {
                       let menuStr = ic.applyCommandCls.getMenuFromCmd(cmdStr);
                       $("#" + me.pre + "replay_cmd").html('Cmd: ' + cmdStr);
                       $("#" + me.pre + "replay_menu").html('Menu: ' + menuStr);
+
+                      me.htmlCls.clickMenuCls.setLogCmd(cmdStrOri, true);
+
                       ic.drawCls.draw();
                  }
             });
@@ -58949,7 +58960,7 @@ var icn3d = (function (exports) {
         //even when multiple iCn3D viewers are shown together.
         this.pre = this.cfg.divid + "_";
 
-        this.REVISION = '3.11.1';
+        this.REVISION = '3.11.2';
 
         // In nodejs, iCn3D defines "window = {navigator: {}}"
         this.bNode = (Object.keys(window).length < 2) ? true : false;
